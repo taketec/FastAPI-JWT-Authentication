7902fbeb2a97cb88bb71f9ba510b01c58f629244
raun1997 <raunakmitra7@gmail.com>
2026-06-14 11:24:32 +0530

    Added function docstrings for veryify_password()

--- app/utils.py
+++ app/utils.py
@@ -8,4 +8,7 @@ def get_hashed_password(password: str)->str:
     return password_context.hash(password)
 
 def verify_password(password: str, hashed_pass: str) -> bool:
+    """Takes both the plain and the hashed passwords and 
+    returns a boolean representing whether the two passwords
+    match or not."""
     return password_context.verify(password, hashed_pass)
